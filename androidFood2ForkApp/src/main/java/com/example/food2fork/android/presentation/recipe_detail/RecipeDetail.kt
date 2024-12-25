6a7075a22091273fdf36d644c7ec81b2c814c467
Joel Crawford <joelcrawford@Joels-MacBook-Pro.local>
2024-12-25 17:34:16 +0300

    Recipe list and detail screen

---
+++ androidFood2ForkApp/src/main/java/com/example/food2fork/android/presentation/recipe_detail/RecipeDetail.kt
@@ -0,0 +1,9 @@
+package com.example.food2fork.android.presentation.recipe_detail
+
+import androidx.compose.runtime.Composable
+import com.example.food2fork.android.presentation.navigation.Screen
+
+@Composable
+fun RecipeDetail() {
+
+}
---
+++ androidFood2ForkApp/src/main/java/com/example/food2fork/android/presentation/recipe_list/RecipeListScreen.kt
@@ -0,0 +1,7 @@
+package com.example.food2fork.android.presentation.recipe_list
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun RecipeList(){
+}
