6827e4aa24b2c2b8d8011e32cde6c126e2bc926a
Joel Crawford <joelcrawford@Joels-MacBook-Pro.local>
2024-12-25 15:47:24 +0300

    Removed unused code

--- androidFood2ForkApp/src/main/java/com/example/food2fork/android/MainActivity.kt
+++ androidFood2ForkApp/src/main/java/com/example/food2fork/android/MainActivity.kt
@@ -13,28 +13,14 @@ import com.example.food2fork.Greeting
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            MyApplicationTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    GreetingView(Greeting().greet())
-                }
-            }
-        }
+
     }
 }
 
-@Composable
-fun GreetingView(text: String) {
-    Text(text = text)
-}
+
 
 @Preview
 @Composable
 fun DefaultPreview() {
-    MyApplicationTheme {
-        GreetingView("Hello, Android!")
-    }
+
 }
