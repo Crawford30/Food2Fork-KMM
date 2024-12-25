42baf6544317c15f12e69a82bda4d96212178b83
Joel Crawford <joelcrawford@Joels-MacBook-Pro.local>
2024-12-25 15:58:23 +0300

    Route screen

--- androidFood2ForkApp/src/main/java/com/example/food2fork/android/presentation/navigation/Screen.kt
+++ androidFood2ForkApp/src/main/java/com/example/food2fork/android/presentation/navigation/Screen.kt
@@ -2,6 +2,6 @@ package com.example.food2fork.android.presentation.navigation
 
 
 //MARK:- Sealed class to define which screen you can navigate too and the routes
-sealed class Screen {
+sealed class Screen(val route: String) {
 
 }
