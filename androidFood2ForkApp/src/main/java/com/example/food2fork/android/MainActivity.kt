0770c5f9f46186e771a0a24a99538d3e4942f86b
Joel Crawford <joelcrawford@Joels-MacBook-Pro.local>
2024-12-25 15:55:44 +0300

    Refactored code

--- androidFood2ForkApp/src/main/java/com/example/food2fork/android/MainActivity.kt
+++ androidFood2ForkApp/src/main/java/com/example/food2fork/android/MainActivity.kt
@@ -9,10 +9,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.food2fork.Greeting
+import kotlinx.coroutines.joinAll
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
 
     }
 }
