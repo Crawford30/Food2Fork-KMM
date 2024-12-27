971cfa72f9f46f7ed42beb0211c97a325c7e7321
Joel Crawford <joelcrawford@Joels-MacBook-Pro.local>
2024-12-27 11:40:52 +0300

    Passing data as arguments

--- androidFood2ForkApp/src/main/java/com/example/food2fork/android/presentation/navigation/Navigation.kt
+++ androidFood2ForkApp/src/main/java/com/example/food2fork/android/presentation/navigation/Navigation.kt
@@ -10,6 +10,7 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.food2fork.android.presentation.recipe_detail.RecipeDetailScreen
 import com.example.food2fork.android.presentation.recipe_list.RecipeListScreen
 
 
@@ -30,9 +31,22 @@ val navController = rememberNavController()
         }
 
 
-        composable(route = Screen.RecipeDetail.route){
+        composable(route = Screen.RecipeDetail.route + "/{recipeID}"){
+            //composable(route = Screen.RecipeDetail.route + "/{recipeID}"){ should be the same as
+            // RecipeDetailScreen(recipeID = navBackStackEntry.arguments?.get("recipeID"))
+
+
             //navBackStackEntry->to persist the state in navbackStack
                 navBackStackEntry ->
+
+            // Safely retrieve and parse the recipeID as an Int
+            val recipeID = navBackStackEntry.arguments?.getString("recipeID")?.toIntOrNull()
+
+            RecipeDetailScreen(recipeID = recipeID)
+
+            //Get the recipeID in the detail screen from nav backstack
+
+//            RecipeDetailScreen(recipeID = navBackStackEntry.arguments?.get("recipeID"))
 
 
         }
