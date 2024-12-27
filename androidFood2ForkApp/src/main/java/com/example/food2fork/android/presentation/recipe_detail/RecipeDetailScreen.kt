dda28bd98e630ec1038ec6e1f56308394e6539c4
Joel Crawford <joelcrawford@Joels-MacBook-Pro.local>
2024-12-27 11:10:45 +0300

    Recipe List screen

--- androidFood2ForkApp/src/main/java/com/example/food2fork/android/presentation/recipe_detail/RecipeDetailScreen.kt
+++ androidFood2ForkApp/src/main/java/com/example/food2fork/android/presentation/recipe_detail/RecipeDetailScreen.kt
@@ -1,9 +1,8 @@
 package com.example.food2fork.android.presentation.recipe_detail
 
 import androidx.compose.runtime.Composable
-import com.example.food2fork.android.presentation.navigation.Screen
 
 @Composable
-fun RecipeDetail() {
+fun RecipeDetailScreen() {
 
 }
--- androidFood2ForkApp/src/main/java/com/example/food2fork/android/presentation/recipe_list/RecipeListScreen.kt
+++ androidFood2ForkApp/src/main/java/com/example/food2fork/android/presentation/recipe_list/RecipeListScreen.kt
@@ -1,7 +1,33 @@
 package com.example.food2fork.android.presentation.recipe_list
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
-fun RecipeList(){
+fun RecipeList(
+    onSelectedRecipe: (Int) -> Unit
+
+){
+    //Use a lazy column..
+    LazyColumn {
+        items(100){ recipeID ->
+            Row(modifier = Modifier
+                .fillParentMaxWidth()
+                .clickable { onSelectedRecipe(recipeID) }
+            ) {
+                Text(
+                    modifier = Modifier.padding(16.dp),
+                    text = "RcipeID = ${recipeID}"
+                )
+            }
+
+        }
+    }
 }
